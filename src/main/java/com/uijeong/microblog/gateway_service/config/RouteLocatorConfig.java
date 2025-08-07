62f23a18aba2693a1fb02d47e68781a59c768654
JeongUijeong <jeong275117@gmail.com>
2025-08-07 20:44:35 +0900

    feat: java DSL 방식의 라우팅 설정 추가

---
+++ src/main/java/com/uijeong/microblog/gateway_service/config/RouteLocatorConfig.java
@@ -0,0 +1,19 @@
+package com.uijeong.microblog.gateway_service.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteLocatorConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route("auth-service", r -> r.path("/api/auth/**").uri("lb://auth-service"))
+            .route("member-service", r -> r.path("/api/members/**").uri("lb://member-service"))
+            .route("post-service", r -> r.path("/api/posts/**").uri("lb://post-service")).build();
+    }
+}
+
